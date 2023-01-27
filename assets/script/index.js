48c1e4597be10bbce0f42daa69edafbcba23071d
AlikZ0 <zalinov96@gmail.com>
2023-01-27 20:40:00 +0400

    message

---
+++ assets/script/index.js
@@ -0,0 +1,35 @@
+// function foo(){
+
+//     var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//     var passwordLength =17;
+//     var password = "";
+       
+//     for (var i = 0; i <= passwordLength; i++) {
+//       var randomNumber = Math.floor(Math.random() * chars.length);
+//       password += chars.substring(randomNumber, randomNumber +1);
+//      }
+      
+//       return password
+//     }
+//     let x=foo()
+//     console.log(x)
+
+
+var password=document.getElementById("password");
+
+ function genPassword() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 12;
+    var password = "";
+ for (var i = 0; i <= passwordLength; i++) {
+   var randomNumber = Math.floor(Math.random() * chars.length);
+   password += chars.substring(randomNumber, randomNumber +1);
+  }
+        document.getElementById("password").value = password;
+ }
+
+function copyPassword() {
+  var copyText = document.getElementById("password");
+  copyText.select();
+  document.execCommand("copy");  
+}
